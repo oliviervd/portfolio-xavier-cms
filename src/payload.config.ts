99193be0f4aa9dc948b7cb1a8767f0c483d96d7d
oliviervd <oliviervandhuynslager@gmail.com>
2023-10-21 19:43:02 +0200

    add 3D connection for media storafge

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,12 +1,14 @@
-import path from 'path'
+import {cloudStorage} from "@payloadcms/plugin-cloud-storage";
+import {s3Adapter} from "@payloadcms/plugin-cloud-storage/s3";
+import path from "path";
 
-import { payloadCloud } from '@payloadcms/plugin-cloud'
-import { mongooseAdapter } from '@payloadcms/db-mongodb'
-import { webpackBundler } from '@payloadcms/bundler-webpack'
-import { slateEditor } from '@payloadcms/richtext-slate'
-import { buildConfig } from 'payload/config'
+import { payloadCloud } from "@payloadcms/plugin-cloud";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { webpackBundler } from "@payloadcms/bundler-webpack";
+import { slateEditor } from "@payloadcms/richtext-slate";
+import { buildConfig } from "payload/config";
 
-import Users from './collections/Users'
+import Users from "./collections/Users";
 
 export default buildConfig({
   admin: {
@@ -16,13 +18,36 @@ export default buildConfig({
   editor: slateEditor({}),
   collections: [Users],
   typescript: {
-    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
   },
   graphQL: {
-    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+    schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
   },
-  plugins: [payloadCloud()],
-  db: mongooseAdapter({
-    url: process.env.DATABASE_URI,
-  }),
-})
+  plugins: [
+    cloudStorage({
+      collections: {
+        media: {
+          adapter: s3Adapter({
+            config: {
+              endpoint: process.env.S3_ENDPOINT,
+              region: process.env.S3_REGION,
+              credentials: {
+                accessKeyId: process.env.S3_ACCESS_KEY_ID,
+                secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+              },
+            },
+            bucket: process.env.S3_BUCKET,
+          }),
+        },
+      },
+    }), 
+    db: mongooseAdapter({
+      url: process.env.DATABASE_URI
+    })
+  ],
+  serverURL: process.env.PAYLOAD_URL,
+  // CORS
+  cors: ["http://localhost:3000", "https://p02--admin--wjrlktvqwrfv.code.run/"],
+  csrf: ["http://localhost:3000", "https://p02--admin--wjrlktvqwrfv.code.run/"],
+
+});
